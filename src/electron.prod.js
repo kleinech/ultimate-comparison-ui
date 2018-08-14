d4b2921c5fcd975b32f8fe71fdbbd972a9a9e174
Christoph Kleine <kleinech.github@gmail.com>
2018-08-14 13:44:57 +0200

    Fix windows issues
    Signed-off-by: Christoph Kleine <kleinech.github@gmail.com>

--- src/electron.prod.js
+++ src/electron.prod.js
@@ -1,11 +1,11 @@
 const isWin = process.platform === "win32";
-if (handleSquirrelEvent()) {
-  return;
-}
-
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+
+if (handleSquirrelEvent()) {
+  return;
+}
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
